2b2cf39c364f1977d2e909f9228f3b0b5eced421
ajinalem <melanija.grunte@gmail.com>
2017-07-20 19:22:01 +0300

    initial commit

---
+++ 2. tic tac toe/GameState.swift
@@ -0,0 +1,79 @@
+enum Move {
+    case x
+    case o
+}
+
+struct GameState {
+    var state:[[Move?]]
+    
+    init(count: Int) {
+        state = Array(repeating: Array(repeating: nil , count: count), count: count)
+    }
+    
+    mutating func addMove(row:Int, column:Int, move:Move) -> Bool {
+        guard state[row][column] == nil else {
+            return false
+        }
+        state[row][column] = move
+        return true
+    }
+    
+    func hasWinner() -> Move? {
+        let transposedState = transpose(input: state)
+        
+        
+        if state.contains(where: { $0.every { $0 == .x } })
+            || transposedState.contains(where: { $0.every { $0 == .x } }){
+            return .x
+        }
+        
+        if state.contains(where: { $0.every { $0 == .o } })
+            || transposedState.contains(where: { $0.every { $0 == .o } }) {
+            return .o
+        }
+    
+    
+        
+        // TODO: Check diaglonal winner
+        // pirmais masīva lodziņš + 1 katrai nākamajai rindai / pēdējais masīva lodziņs - 1 katrai nākamajai rindai
+        
+        
+        
+        // Return nil if we have no winner
+        return nil
+    }
+}
+
+//        [1][2][3]-
+//        [4][5][6]-
+//        [7][8][9]-
+//         |  |  |
+
+public func transpose<T>(input: [[T]]) -> [[T]] {
+    if input.isEmpty { return [[T]]() }
+    let count = input[0].count
+    
+    
+    var out = Array(repeating: [T](), count: count)
+    for outer in input {
+        for (index, inner) in outer.enumerated() {
+            out[index].append(inner)
+        }
+    }
+    
+    return out
+}
+
+extension Sequence {
+    /// Returns `true` if *every* element in `self` satisfies `predicate`.
+    public func every(
+        predicate: (Self.Iterator.Element) throws -> Bool
+        ) rethrows -> Bool {
+        for element in self {
+            if try !predicate(element) {
+                return false
+            }
+        }
+        return true
+    }
+}
---
+++ 2. tic tac toe/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  2. tic tac toe
+//
+//  Created by Melanija Grunte on 13/07/2017.
+//  Copyright © 2017 Melanija Grunte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func loadView() {
+        view = UIView()
+        
+        view.backgroundColor = UIColor.black
+        
+        
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("World", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.addTarget(self, action: #selector(ViewController.tap), for: .touchUpInside)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 87)
+        return button
+    }()
+    
+    
+    func tap() {
+        button.setTitle("Mela", for: .normal)
+    }
+}
+
+
+class GameSpaceView: UIView {
+    
+    let move: Move?
+    
+    init(move: Move? = nil) {
+        self.move = move
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        
+    }
+    
+    
+}
